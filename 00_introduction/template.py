34770ccede810dce3dcf952c6ca6a6b73e395e8c
Axel Ólafsson <axelola18@ru.is>
2021-08-17 16:25:31 +0000

    Part 1, intro assignment

--- 00_introduction/template.py
+++ 00_introduction/template.py
@@ -4,24 +4,43 @@ import matplotlib.pyplot as plt
 
 def normal(x: np.ndarray, sigma: float, mu: float) -> np.ndarray:
     # Part 1.1
-
+    t = np.exp(-(np.power((x-mu),2))/(2*np.power(sigma, 2)))
+    n = np.sqrt(2*np.pi*np.power(sigma, 2))
+    p = t/n
+    return p
 def plot_normal(sigma: float, mu:float, x_start: float, x_end: float):
     # Part 1.2
+    x_range = np.linspace(x_start, x_end, 500)
+    distribution = normal(x_range, sigma, mu)
+    plt.plot(x_range, distribution)
 
 def _plot_three_normals():
     # Part 1.2
-
+    return 1
 def normal_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
     # Part 2.1
+    n = len(x)
+    gaussianSum = 0
+    for i in range(n):
+        density_i = weights[i] * normal(x, sigmas[i], mus[i])
+        gaussianSum += density_i
+    return gaussianSum
 
 def _compare_components_and_mixture():
     # Part 2.2
-
+    return 1
 def sample_gaussian_mixture(sigmas: list, mus: list, weights: list, n_samples: int = 500):
     # Part 3.1
-
+    return 1
 def _plot_mixture_and_samples():
     # Part 3.2
-
+    return 1
 if __name__ == '__main__':
     # select your function to test here and do `python3 template.py`
+    plot_normal(0.5, 0, -5, 5)
+    plot_normal(0.25, 1, -5, 5)
+    plot_normal(1, 1.5, -5, 5)
+    plt.show()
+
+    out = normal_mixture(np.linspace(-5, 5, 5), [0.5, 0.25, 1], [0, 1, 1.5], [1/3, 1/3, 1/3])
+    print(out)
