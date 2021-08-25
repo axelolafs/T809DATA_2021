d673f893f453c25245685c0ce755b317f4bf360f
axelolafs <axelola18@ru.is>
2021-08-25 21:06:42 +0000

    Part 1

--- 01_decision_trees/template.py
+++ 01_decision_trees/template.py
@@ -35,13 +35,23 @@ def split_data(
     Split a dataset and targets into two seperate datasets
     where data with split_feature < theta goes to 1 otherwise 2
     '''
-    features_1 = features[...]
-    targets_1 = targets[...]
+    
+    N = len(features)
+    features_1 = []
+    targets_1 = []
 
-    features_2 = features[...]
-    targets_2 = targets[...]
+    features_2 = []
+    targets_2 = []
 
-    return (features_1, targets_1), (features_2, targets_2)
+    for i in range(N):
+        if(features[i][split_feature_index] < theta):
+            features_1.append(features[i])
+            targets_1.append(targets[i])
+        else:
+            features_2.append(features[i])
+            targets_2.append(targets[i])
+
+    return (np.array(features_1), np.array(targets_1)), (np.array(features_2), np.array(targets_2))
 
 
 def gini_impurity(targets: np.ndarray, classes: list) -> float:
@@ -49,7 +59,10 @@ def gini_impurity(targets: np.ndarray, classes: list) -> float:
     Calculate:
         i(S_k) = 1/2 * (1 - sum_i P{C_i}**2)
     '''
-    ...
+    squarelist = np.power(prior(targets, classes), 2)
+    sum = squarelist.sum()
+
+    return 0.5*(1 - sum)
 
 
 def weighted_impurity(
@@ -61,10 +74,14 @@ def weighted_impurity(
     Given targets of two branches, return the weighted
     sum of gini branch impurities
     '''
-    g1 = gini_impurity(...)
-    g2 = gini_impurity(...)
+    g1 = gini_impurity(t1, classes)
+    g2 = gini_impurity(t2, classes)
     n = t1.shape[0] + t2.shape[0]
-    ...
+    
+    n1 = len(t1)
+    n2 = len(t2)
+
+    return ((n1*g1) + (n2*g2)) / n
 
 
 def total_gini_impurity(
@@ -78,8 +95,8 @@ def total_gini_impurity(
     Calculate the gini impurity for a split on split_feature_index
     for a given dataset of features and targets.
     '''
-    ...
-
+    (_, t1), (_, t2) = split_data(features, targets, split_feature_index, theta)
+    return weighted_impurity(t1, t2, classes)
 
 def brute_best_split(
     features: np.ndarray,
@@ -98,12 +115,28 @@ def brute_best_split(
     # iterate feature dimensions
     for i in range(features.shape[1]):
         # create the thresholds
-        thetas = ...
+        thetas = exclusive_interval(features[:,i], num_tries)
         # iterate thresholds
         for theta in thetas:
-            ...
+            current_gini = total_gini_impurity(features, targets, classes, i, theta)
+            if(current_gini < best_gini):
+                best_gini = current_gini
+                best_dim = i
+                best_theta = theta
+
     return best_gini, best_dim, best_theta
 
+def exclusive_interval(values: np.ndarray, num_points: int) -> np.ndarray:
+    '''
+    Create 50 linearly spaced values between the minimum
+    and maximum values in a given list. We also want to
+    exclude the min and the max in the interval.
+    '''
+    min_value = values.min()
+    max_value = values.max()
+
+    interval = np.linspace(min_value, max_value, num_points+2)[1:-1]
+    return interval
 
 class IrisTreeTrainer:
     def __init__(
@@ -125,7 +158,7 @@ class IrisTreeTrainer:
         self.tree = DecisionTreeClassifier()
 
     def train(self):
-        ...
+        return self.tree.fit(self.train_features,self.train_targets)
 
     def accuracy(self):
         ...
@@ -145,11 +178,5 @@ class IrisTreeTrainer:
         ...
 
 
-print(prior([0, 0, 1], [0, 1]))
-print(prior([0, 2, 3, 3], [0, 1, 2, 3]))
-# arr = [2, 3, 4, 5, 3, 4, 5, 3, 5, 4, 7, 8, 3, 6, 2]
-# print('Numpy Array:')
-# print(arr)
-# # Count occurrence of element '3' in numpy array
-# count = (arr == 3).sum()
-# print('Total occurences of "3" in array: ', count)
+features, targets, classes = load_iris()
+print(brute_best_split(features, targets, classes, 30))
