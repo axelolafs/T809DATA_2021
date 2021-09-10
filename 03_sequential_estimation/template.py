e920b8abdec3629ff68665e65c3ad633fe641ea2
axelolafs <axelola18@ru.is>
2021-09-10 13:50:56 +0000

    ass

--- 03_sequential_estimation/template.py
+++ 03_sequential_estimation/template.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import identity
 from tools import scatter_3d_data, bar_per_axis
 
 import matplotlib.pyplot as plt
@@ -13,8 +14,14 @@ def gen_data(
     '''Generate n values samples from the k-variate
     normal distribution
     '''
-    ...
 
+    X = []
+    Ik = identity(k)
+    cov = np.power(var, 2) * Ik
+    for i in range(n):
+        X.append(np.random.multivariate_normal(mean, cov))
+
+    return np.array(X)
 
 def update_sequence_mean(
     mu: np.ndarray,
@@ -23,26 +30,42 @@ def update_sequence_mean(
 ) -> np.ndarray:
     '''Performs the mean sequence estimation update
     '''
-    ...
+    return mu + (1/n)*(x-mu)
 
 
 def _plot_sequence_estimate():
-    data = ...
+    data = gen_data(100, 3, np.array([0, 0, 0]), 1.3)
     estimates = [np.array([0, 0, 0])]
     for i in range(data.shape[0]):
-        ...
+        estimates.append(update_sequence_mean(estimates[i], data[i], i+1))
+
+    # print(estimates)
     plt.plot([e[0] for e in estimates], label='First dimension')
-    ...
+    plt.plot([e[1] for e in estimates], label='Second dimension')
+    plt.plot([e[2] for e in estimates], label='Third dimension')
     plt.legend(loc='upper center')
+    plt.xlabel('Iteration')
+    plt.ylabel('Estimated mean')
     plt.show()
+    return estimates
 
 
 def _square_error(y, y_hat):
-    ...
+    return (y - y_hat)**2
 
 
 def _plot_mean_square_error():
-    ...
+    estimates = _plot_sequence_estimate()
+    sq_err = []
+    for i in range(100):
+        err = (_square_error(estimates[i][0], 0) + _square_error(estimates[i][1], 0) + _square_error(estimates[i][2], 0))/3
+        sq_err.append(err)
+    
+    plt.plot(sq_err)
+    plt.xlabel('Iteration')
+    plt.ylabel('Mean square error')
+    plt.show()
+
 
 
 # Naive solution to the independent question.
@@ -55,9 +78,22 @@ def gen_changing_data(
     var: float
 ) -> np.ndarray:
     # remove this if you don't go for the independent section
-    ...
+    X = []
+    Ik = identity(k)
+    cov = np.power(var, 2) * Ik
+    mean = np.linspace(start_mean, end_mean, n)
+    for i in range(n):
+        X.append(np.random.multivariate_normal(mean[i], cov))
+
+    return np.array(X)  
 
 
 def _plot_changing_sequence_estimate():
     # remove this if you don't go for the independent section
     ...
+
+def main():
+    _plot_mean_square_error()
+
+if __name__ == '__main__':
+    main()
--- 04_classification/template.py
+++ 04_classification/template.py
@@ -3,6 +3,7 @@ from tools import load_iris, split_train_test
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import multivariate_normal
+from sklearn.metrics import accuracy_score
 
 
 def mean_of_class(
@@ -14,7 +15,12 @@ def mean_of_class(
     Estimate the mean of a selected class given all features
     and targets in a dataset
     '''
-    ...
+    features_of_class = []
+    for i in range(len(targets)):
+        if targets[i] == selected_class:
+            features_of_class.append(features[i])
+    return np.mean(features_of_class, axis=0)
+
 
 
 def covar_of_class(
@@ -26,7 +32,11 @@ def covar_of_class(
     Estimate the covariance of a selected class given all
     features and targets in a dataset
     '''
-    ...
+    features_of_class = []
+    for i in range(len(targets)):
+        if targets[i] == selected_class:
+            features_of_class.append(features[i])
+    return np.cov(features_of_class, rowvar=False)
 
 
 def likelihood_of_class(
@@ -39,7 +49,8 @@ def likelihood_of_class(
     from a multivariate normal distribution, given the mean
     and covariance of the distribution.
     '''
-    ...
+    return multivariate_normal(mean=class_mean
+    , cov=class_covar).pdf(feature)
 
 
 def maximum_likelihood(
@@ -59,10 +70,14 @@ def maximum_likelihood(
     '''
     means, covs = [], []
     for class_label in classes:
-        ...
+        means.append(mean_of_class(train_features, train_targets, class_label))
+        covs.append(covar_of_class(train_features, train_targets, class_label))
     likelihoods = []
     for i in range(test_features.shape[0]):
-        ...
+        vec = []
+        for j in range(len(classes)):
+            vec.append(likelihood_of_class(test_features[i], means[j], covs[j]))
+        likelihoods.append(vec)
     return np.array(likelihoods)
 
 
@@ -75,7 +90,7 @@ def predict(likelihoods: np.ndarray):
     You should return a [likelihoods.shape[0]] shaped numpy
     array of predictions, e.g. [0, 1, 0, ..., 1, 2]
     '''
-    ...
+    return np.argmax(likelihoods, axis=1)
 
 
 def maximum_aposteriori(
@@ -93,4 +108,53 @@ def maximum_aposteriori(
     a [test_features.shape[0] x len(classes)] shaped numpy
     array
     '''
-    ...
+    means, covs = [], []
+    for class_label in classes:
+        means.append(mean_of_class(train_features, train_targets, class_label))
+        covs.append(covar_of_class(train_features, train_targets, class_label))
+    likelihoods = []
+    count_class = np.bincount(train_targets)
+    print(count_class)
+    for i in range(test_features.shape[0]):
+        vec = []
+        for j in range(len(classes)):
+            scaler = count_class[j]/len(train_targets)
+
+            vec.append(likelihood_of_class(test_features[i], means[j], covs[j])* scaler)
+        likelihoods.append(vec)
+    return np.array(likelihoods)
+
+def accuracy(test_targets, likelihoods):
+    prediction = predict(likelihoods)
+
+    return accuracy_score(test_targets, prediction)
+
+def confusion_matrix(classes, test_targets, likelihoods):
+    n = len(classes)
+    matrix = np.zeros((n, n))
+    N = len(test_targets)
+    guessVector = predict(likelihoods)
+
+    for k in range(N):
+        i = test_targets[k]
+        j = guessVector[k]
+        matrix[i][j] += 1
+    return matrix
+
+def main():
+    features, targets, classes = load_iris()
+    (train_features, train_targets), (test_features, test_targets)\
+    = split_train_test(features, targets, train_ratio=0.6)
+    mle = maximum_likelihood(train_features, train_targets, test_features, classes)
+    map = maximum_aposteriori(train_features, train_targets, test_features, classes)
+    print(mle)
+    print(map)
+    
+    print(accuracy(test_targets, mle))
+    print(accuracy(test_targets, map))
+
+    print(confusion_matrix(classes, test_targets, mle))
+    print(confusion_matrix(classes, test_targets, map))
+
+if __name__ == '__main__':
+    main()
